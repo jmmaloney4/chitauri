8d49387e7e0d5e41949b3b0375adc0dbf570efc6
Jack Maloney <jmmaloney4@gmail.com>
2021-03-03 17:43:58 -0600

    Actually fills in torrent data from file

--- torrent/torrent.go
+++ torrent/torrent.go
@@ -48,18 +48,45 @@ func (i *bencodeInfo) splitPieceHashes() ([][sha1.Size]byte, error) {
 	return hashes, nil
 }
 
+func (i *bencodeInfo) sha1sum() ([sha1.Size]byte, error) {
+	bytes, err := bencode.Marshal(*i)
+	if err != nil {
+		return [sha1.Size]byte{}, err
+	}
+	h := sha1.Sum(bytes)
+	return h, nil
+}
+
 func NewTorrentFile(file io.Reader) (*TorrentFile, error) {
-	rv := new(TorrentFile)
 	in := new(bytes.Buffer)
 	in.ReadFrom(file)
 
-	data := bencodeTorrent{}
-	err := bencode.Unmarshal(in.Bytes(), &data)
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(in.Bytes(), &bto)
 	if err != nil {
 		return nil, err
 	}
 
-	return rv, nil
+	infoHash, err := bto.Info.sha1sum()
+	if err != nil {
+		return nil, err
+	}
+
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return nil, err
+	}
+
+	rv := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+
+	return &rv, nil
 }
 
 func TorrentFileAtURL(url string) (*TorrentFile, error) {
